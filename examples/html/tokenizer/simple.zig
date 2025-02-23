cf378ebc8cb26d288511f15616f7c58c610dceea
doccaico <hash_ext_luna@yahoo.co.jp>
2025-02-23 18:47:04 +0900

    clean up

--- examples/html/tokenizer/simple.zig
+++ examples/html/tokenizer/simple.zig
@@ -65,7 +65,8 @@ fn tokenCallback(tkz: ?*html.Tokenizer, token: ?*html.Token, ctx: ?*anyopaque) c
         // Value
         if (attr.?.value_begin != null) {
             // Get original quote
-            const qo: core.CharType = @as(*core.CharType, @ptrFromInt((@intFromPtr(attr.?.value_begin) - 1))).*;
+            // const qo: core.CharType = @as(*core.CharType, @ptrFromInt((@intFromPtr(attr.?.value_begin) - 1))).*;
+            const qo: core.CharType = (@intFromPtr(attr.?.value_begin.?) - 1).*;
 
             // Attribute have no quote
             if (qo == '=') {
